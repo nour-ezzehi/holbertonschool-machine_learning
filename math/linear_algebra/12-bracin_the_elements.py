26e9aa0b18c7610c329541f7441eb26891e01011
nour-ezzehi <6868@holbertonstudents.com>
2024-04-15 19:44:51 +0100

    pycode

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,5 +3,7 @@
 
 
 def np_elementwise(mat1, mat2):
-    """performs element-wise addition, subtraction, multiplication, and division"""
+    """performs element-wise addition, subtraction,
+    multiplication, and division
+    """
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
