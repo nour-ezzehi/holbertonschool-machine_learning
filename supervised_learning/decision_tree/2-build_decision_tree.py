947ac79f9c0a21cf9af7e035f59a4cd094a04f7c
nour-ezzehi <6868@holbertonstudents.com>
2024-04-28 10:26:48 +0100

    output fixed

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -70,9 +70,9 @@ class Node:
             "    +--"
             + lines[0]
             + "\n"
-            + "\n".join("    " + " " * 3 + line for line in lines[1:])[:-1]  # Remove the last newline character
-        )
-
+            + "\n".join("    " + " " * 3 + line for line in lines[1:])
+        ).rstrip()
+  
     def __str__(self):
         """Return the Node and its children"""
         result = (
