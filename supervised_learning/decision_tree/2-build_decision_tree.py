7a2e3152e96b583998eb8c6ee5dac1dd834f97a6
nour-ezzehi <6868@holbertonstudents.com>
2024-04-28 10:16:34 +0100

    output fixed

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -107,7 +107,7 @@ class Leaf(Node):
 
     def __str__(self):
         """print leaf"""
-        return (f"-> leaf [value={self.value}] ")
+        return (f"-> leaf [value={self.value}]")
 
 
 class Decision_Tree():
