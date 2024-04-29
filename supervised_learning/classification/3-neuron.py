74c9763160bb62add479d2e52d68d67b72bd4b44
nour-ezzehi <6868@holbertonstudents.com>
2024-04-29 21:05:55 +0100

    logistic regression

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""binary classification"""
+
+import numpy as np
+
+
+class Neuron():
+    """neuron class"""
+
+    def __init__(self, nx):
+        """constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter function for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter function for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter function for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation function"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+
+        return self.__A
+
+    def cost(self, Y, A):
+        """neuron cost"""
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+
+        return cost
