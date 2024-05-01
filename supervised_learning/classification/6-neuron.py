20527368654f263071414cfba6aa83554ee4e2b2
nour-ezzehi <6868@holbertonstudents.com>
2024-05-01 13:03:39 +0100

    train neuron

---
+++ supervised_learning/classification/6-neuron.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""binary classification"""
+
+import numpy as np
+
+
+class Neuron():
+    """neuron class"""
+
+    def __init__(self, nx):
+        """constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter function for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter function for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter function for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation function"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+
+        return self.__A
+
+    def cost(self, Y, A):
+        """neuron cost"""
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluate Neuron"""
+        self.forward_prop(X)
+        cost = self.cost(Y, self.__A)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """gradient descent"""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = np.matmul(X, dZ.T) / m
+        db = np.sum(dZ) / m
+        self.__W = self.__W - alpha * dW.T
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """trains a neuron"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+        for _ in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha)
+
+        return self.evaluate(X, Y)
