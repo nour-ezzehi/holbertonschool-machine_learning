0e32a8770fa033fc026e3b95621e2bd8825b444b
nour-ezzehi <6868@holbertonstudents.com>
2024-05-14 16:33:09 +0100

    pycode

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -46,5 +46,3 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
             if step < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         return saver.save(sess, save_path)
-
-
