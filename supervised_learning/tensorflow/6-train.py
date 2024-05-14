fc0a2db61a0e348cb214669579afc889540422bc
nour-ezzehi <6868@holbertonstudents.com>
2024-05-14 14:36:01 +0100

    output fixed

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -37,7 +37,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                                      feed_dict={x: X_train, y: Y_train})
             cost_v, acc_v = sess.run([loss, accuracy],
                                      feed_dict={x: X_valid, y: Y_valid})
-            if step % 100 == 0 or i == iterations:
+            if step % 100 == 0 or step == iterations:
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(cost_t))
                 print("\tTraining Accuracy: {}".format(acc_t))
@@ -45,5 +45,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                 print("\tValidation Accuracy: {}".format(acc_v))
             if step < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+
 
     return saver.save(sess, save_path)
