e32ad02d7cb7cc6bc42101564b653dddeaf27c9f
nour-ezzehi <6868@holbertonstudents.com>
2024-06-03 22:16:04 +0100

    fix import

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """ 1. Input """
 
-import tensorflow.keras as k
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ builds a neural network with the Keras library """
 
-    inputs = k.Input(shape=(nx,))
+    inputs = K.Input(shape=(nx,))
     x = inputs
-    regularizer = k.regularizers.l2(lambtha)
+    regularizer = K.regularizers.l2(lambtha)
 
     for i, (layer_size, activation) in enumerate(zip(layers, activations)):
-        x = k.layers.Dense(
+        x = K.layers.Dense(
             layer_size,
             activation=activation,
             kernel_regularizer=regularizer)(x)
         if i < len(layers) - 1:
-            x = k.layers.Dropout(1 - keep_prob)(x)
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
-    model = k.Model(inputs=inputs, outputs=x)
+    model = K.Model(inputs=inputs, outputs=x)
 
     return model
