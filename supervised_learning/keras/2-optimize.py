8e82ab399fdaa23cc32bac96afd6111d3f58ce12
nour-ezzehi <6868@holbertonstudents.com>
2024-06-03 23:03:35 +0100

    fix optimizer parameter

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -5,9 +5,8 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """ sets up Adam optimization for a keras model with
-    categorical crossentropy loss and accuracy metrics"""
-    network.compile(optimizer=K.optimizers.Adam(lr=alpha,
+    """ sets up Adam optimization for a keras model with categorical crossentropy loss and accuracy metrics"""
+    network.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
                                                 beta_1=beta1,
                                                 beta_2=beta2),
                     loss='categorical_crossentropy',
