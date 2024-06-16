d6b7d459f65820831919ea83c389572b1ae58b9d
nour-ezzehi <6868@holbertonstudents.com>
2024-06-16 19:40:11 +0100

    fix initializations

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -10,29 +10,25 @@ from tensorflow import keras as K
 def identity_block(A_prev, filters):
     """ builds an identity block """
 
+    init = K.initializers.he_normal()
     F11, F3, F12 = filters
-  
-    c1 = K.layers.Conv2D(
-        filters=F11, kernel_size=(
-            1, 1), padding='same', strides=(
-            1, 1), kernel_initializer='he_normal')(A_prev)
-    b1 = K.layers.BatchNormalization(axis=3)(c1)
-    r1 = K.layers.Activation('relu')(b1)
 
-    c2 = K.layers.Conv2D(
-        filters=F3, kernel_size=(
-            3, 3), padding='same', strides=(
-            1, 1), kernel_initializer='he_normal')(r1)
-    b2 = K.layers.BatchNormalization(axis=3)(c2)
-    r2 = K.layers.Activation('relu')(b2)
+    c_F11 = K.layers.Conv2D(F11, kernel_size=1, padding="same",
+                            kernel_initializer=init)(A_prev)
+    norm_F11 = K.layers.BatchNormalization()(c_F11)
+    act_F11 = K.layers.Activation("relu")(norm_F11)
 
-    c3 = K.layers.Conv2D(
-        filters=F12, kernel_size=(
-            1, 1), padding='same', strides=(
-            1, 1), kernel_initializer='he_normal')(r2)
-    b3 = K.layers.BatchNormalization(axis=3)(c3)
-    add = K.layers.Add()([b3, A_prev])
-    
-    output = K.layers.Activation('relu')(add)
+    c_F3 = K.layers.Conv2D(F3, kernel_size=3, padding="same",
+                           kernel_initializer=init)(act_F11)
+    norm_F3 = K.layers.BatchNormalization()(c_F3)
+    act_F3 = K.layers.Activation("relu")(norm_F3)
+
+    c_F12 = K.layers.Conv2D(F12, kernel_size=1, padding="same",
+                            kernel_initializer=init)(act_F3)
+    norm_F12 = K.layers.BatchNormalization()(c_F12)
+
+    X = K.layers.Add()([norm_F12, A_prev])
+
+    output = K.layers.Activation("relu")(X)
 
     return output
