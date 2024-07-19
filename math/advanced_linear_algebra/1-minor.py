bc78494caaccc8af327c48b3607432795c1c8da6
nour-ezzehi <6868@holbertonstudents.com>
2024-07-19 22:16:24 +0100

    fix pycode

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -8,7 +8,7 @@ def determinant(matrix):
         return matrix[0][0]
     if len(matrix) == 2:
         det = ((matrix[0][0] * matrix[1][1])
-                  - (matrix[0][1] * matrix[1][0]))
+               - (matrix[0][1] * matrix[1][0]))
         return det
 
     det = 0
