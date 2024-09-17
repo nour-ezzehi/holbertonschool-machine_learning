8b6d3caa2766877abc6c5ff25b97cd320b0c26b7
nour-ezzehi <6868@holbertonstudents.com>
2024-09-17 12:00:29 +0100

    fix pycode

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -22,7 +22,8 @@ class GaussianProcess:
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
     def predict(self, X_s):
-        """predicts the mean and standard deviation of points in a Gaussian process"""
+        """predicts the mean and standard deviation of
+        points in a Gaussian process"""
 
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
